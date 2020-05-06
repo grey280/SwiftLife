82b65a2414bd8cb5d0797d77200a10cb75dafe90
Grey Patterson <greypatterson@gmail.com>
2020-05-05 20:50:40 -0700

    Saving this because Swift Playgrounds is a weird app

---
+++ Swift Student Submission.playgroundbook/Edits/UserEdits.diffpack/UserModules/UserModule.playgroundmodule/Sources/Grid.swift
@@ -0,0 +1,129 @@
+import SwiftUI
+
+enum GridState{
+    case dead, alive, minusOne, minusTwo, minusThree, minusFour
+}
+
+public class Grid: ObservableObject {
+    private var state: [[GridState]]
+    var wrap: Bool
+    
+    public init?(width: Int, height: Int){
+        if (width <= 0 || height <= 0){
+            return nil
+        }
+        state = [[GridState]](repeating: [GridState](repeating: .dead, count: height), count: width)
+//          cells = [[Bool]](repeating: [Bool](repeating: false, count: y), count: x)
+        
+        wrap = false
+    }
+    
+    /// The width of the grid
+    var width: Int{
+        state.count
+    }
+    /// The height of the grid
+    var height: Int{
+        width > 0 ? state[0].count : 0
+    }
+    
+    /// Iterate the grid using the standard rules of cellular automata/Conway's Game of Life
+    func iterate(){
+        var newCells = state // array of value type is a value type, with copy-on-write
+        for x in 0..<newCells.count{
+            for y in 0..<newCells[x].count{
+                newCells[x][y] = iteratedCellState(x: x, y: y)
+            }
+        }
+        objectWillChange.send()
+        state = newCells
+    }
+    
+    /// Get the value of the cell at the given coordinates.
+    ///
+    /// - Parameters:
+    ///   - x: x-coordinate to use. Zero-based.
+    ///   - y: y-coordinate to use. Zero-based.
+    /// - Returns: whether or not the cell at the given coordinates is 'alive'
+    func cell(x: Int, y: Int) -> GridState{
+        var nX = x, nY = y
+        if wrap{
+            assertionFailure()
+            if x>=width{
+                nX = 0
+            }
+            if x<0{
+                nX = width-1
+            }
+            if y>=height{
+                nY = 0
+            }
+            if y<0{
+                nY = height-1
+            }
+        }else{
+            if (x>width - 1 || x<0 || y>height - 1 || y<0){
+                return .dead
+            }
+        }
+        return state[nX][nY]
+    }
+    
+    private func decreaseState(_ from: GridState) -> GridState {
+        switch from {
+            case .alive:
+            return .minusOne
+            case .minusOne:
+            return .minusTwo
+            case .minusTwo:
+            return .minusThree
+            case .minusThree:
+            return .minusFour
+            case .minusFour:
+            return .minusFour // so we never fully fade out after having been alive - leaves nice trails, ideally
+            case .dead:
+            return .dead
+        }
+    }
+    
+    private func iteratedCellState(x: Int, y: Int) -> GridState {
+        var neighborLiveCount = 0
+        let currentState = cell(x: x, y: y)
+        
+        if cell(x: x-1, y: y-1) == .alive{
+            neighborLiveCount += 1
+        }
+        if cell(x: x, y: y-1) == .alive{
+            neighborLiveCount += 1
+        }
+        if cell(x: x+1, y: y-1) == .alive{
+            neighborLiveCount += 1
+        }
+        if cell(x: x-1, y: y) == .alive{
+            neighborLiveCount += 1
+        }
+        if cell(x: x+1, y: y) == .alive{
+            neighborLiveCount += 1
+        }
+        if cell(x: x-1, y: y+1) == .alive{
+            neighborLiveCount += 1
+        }
+        if cell(x: x, y: y+1) == .alive{
+            neighborLiveCount += 1
+        }
+        if cell(x: x+1, y: y+1) == .alive{
+            neighborLiveCount += 1
+        }
+        
+        switch neighborLiveCount {
+        case 0,1: // If they have fewer than two neighbors, they die of starvation
+            return decreaseState(currentState)
+        case 2: // If they have two neighbors and are alive, they stay alive
+            return currentState
+        case 3: // If they have three neighbors, they either stay alive or one is 'born' there.
+            return .alive
+        default: // If they have more than three neighbors, they die of overpopulation
+            return decreaseState(currentState)
+        }
+    }
+}
---
+++ Swift Student Submission.playgroundbook/Edits/UserEdits.diffpack/UserModules/UserModule.playgroundmodule/Sources/GridView.swift
@@ -0,0 +1,49 @@
+
+import SwiftUI
+
+extension Color {
+    static func from(state gridState: GridState) -> Color{
+        switch gridState{
+            case .dead:
+            return .black
+            case .alive:
+            return .white
+            case .minusOne:
+            return .init(.sRGB, red: 64/255, green: 188/255, blue: 216/255) // 40BCD8
+            case .minusTwo:
+            return .init(.sRGB, red: 57/255, green: 169/255, blue: 219/255)
+            // 39a9db
+            case .minusThree:
+            return .init(.sRGB, red: 0/255, green: 129/255, blue: 175/255)
+            // 0081af
+        case .minusFour:
+            return .init(.sRGB, red: 38/255, green: 96/255, blue: 164/255)
+            // 2660A4
+        }
+    }
+}
+
+public struct GridView: View {
+    @ObservedObject var grid: Grid
+    
+    public init(grid: Grid){
+        self.grid = grid
+    }
+    
+    let timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
+    
+    public var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<grid.width, id: \.self) { x in
+                VStack(spacing: 0){
+                    ForEach(0..<self.grid.height, id: \.self) { y in 
+                        Rectangle().fill(Color.from(state: self.grid.cell(x: x, y: y)))
+                    }
+                }
+            }
+        }.background(Color.black)
+        .onReceive(timer) {_ in 
+            self.grid.iterate()
+        }
+    }
+}
