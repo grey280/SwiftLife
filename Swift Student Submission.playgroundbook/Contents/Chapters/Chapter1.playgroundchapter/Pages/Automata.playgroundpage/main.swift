85fc5e2d48301099e58e03cae495fb8dfb483e39
grey280 <greypatterson@gmail.com>
2020-05-13 21:03:39 -0700

    Markdown formatting

--- Swift Student Submission.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Automata.playgroundpage/main.swift
+++ Swift Student Submission.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Automata.playgroundpage/main.swift
@@ -5,6 +5,7 @@ import SwiftUI
 /*:
  # What is a cell?
  Think of a cell as a single pixel on one of those road signs: no fancy colors, just 'on' or 'off'.
+ 
  That's what a cell is: a square that's either on or off. (Well, we say 'alive' or 'dead', but same thing, really.)
  */
 
