a5f9b54c632912c2cb069f647a15a798eb4f077c
grey280 <greypatterson@gmail.com>
2020-05-14 19:42:15 -0700

    document

--- Swift Student Submission.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Wolfram.playgroundpage/main.swift
+++ Swift Student Submission.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Wolfram.playgroundpage/main.swift
@@ -3,14 +3,32 @@ import PlaygroundSupport
 import SwiftUI
 //#-end-hidden-code
 /*:
- # What is a cell?
- Think of a cell as a single pixel on one of those road signs: no fancy colors, just 'on' or 'off'.
- 
- That's what a cell is: a square that's either on or off. (Well, we say 'alive' or 'dead', but same thing, really.)
- */
+ Now we're going to play around a bit. Let's feed in a cool pattern, instead of just letting it randomize.
 
-let grid = StaticGrid(width: 5, height: 5)!
-let gridView = GridView(grid: grid)
+ We're going to use Wolfram's [Rule 110](https://mathworld.wolfram.com/Rule110.html). It's a *different* kind of cellular automata; go ahead and tinker with the code below, or just watch it run.
+ */
+//#-editable-code
+
+
+public class WolframGrid: IteratingGrid {
+    required public override init?(width: Int, height: Int){
+        if (width <= 0 || height <= 0){
+            return nil
+        }
+        super.init(width: width, height: height)
+        //#-editable-code
+        for x in 0..<width{
+            for y in 0..<height {
+                self[x, y] = weightedCoinFlip() ? .alive : .dead
+            }
+        }
+        //#-end-editable-code
+    }
+}
+
+let grid = WolframGrid(width: /*#-editable-code*/45/*#-end-editable-code*/, height: /*#-editable-code*/45/*#-end-editable-code*/)!
+let gridView = GridView(grid: grid, tickSpeed: /*#-editable-code*/0.5/*#-end-editable-code*/)
+//#-end-editable-code
 //#-hidden-code
 PlaygroundPage.current.setLiveView(gridView)
 //#-end-hidden-code
