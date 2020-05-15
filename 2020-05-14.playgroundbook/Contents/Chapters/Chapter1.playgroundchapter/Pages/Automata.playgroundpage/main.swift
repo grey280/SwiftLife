b51f08ecd424f57b314b56d3b0dcf0177a44483d
grey280 <greypatterson@gmail.com>
2020-05-14 20:13:06 -0700

    Snapshot again

---
+++ 2020-05-14.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Automata.playgroundpage/main.swift
@@ -0,0 +1,16 @@
+//#-hidden-code
+import PlaygroundSupport
+import SwiftUI
+//#-end-hidden-code
+/*:
+ # What is a cell?
+ Think of a cell as a single pixel on one of those road signs: no fancy colors, just 'on' or 'off'.
+ 
+ That's what a cell is: a square that's either on or off. (Well, we say 'alive' or 'dead', but same thing, really.)
+ */
+
+let grid = StaticGrid(width: 5, height: 5)!
+let gridView = GridView(grid: grid)
+//#-hidden-code
+PlaygroundPage.current.setLiveView(gridView)
+//#-end-hidden-code
